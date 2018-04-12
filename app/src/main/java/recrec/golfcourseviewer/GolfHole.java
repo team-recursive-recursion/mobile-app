41c27a84ed6b080328af02dcf7fbfe081a8543a7
Duncan Tilley <duncan.tilley@gmail.com>
2018-04-13 00:05:10 +0200

    Improve course rendering

--- app/src/main/java/recrec/golfcourseviewer/GolfHole.java
+++ app/src/main/java/recrec/golfcourseviewer/GolfHole.java
@@ -31,31 +31,39 @@ public class GolfHole {
             for (GolfPoint point : poly.getPoints()) {
                 opt.add(new LatLng(point.getLatitude(), point.getLongitude()));
             }
+            float zIndex;
             switch (poly.getType()) {
                 case TYPE_ROUGH:
+                    zIndex = 0.0f;
                     opt.fillColor(res.getColor(R.color.colorRough));
                     opt.strokeColor(res.getColor(R.color.colorRough));
                     break;
                 case TYPE_FAIRWAY:
+                    zIndex = 1.0f;
                     opt.fillColor(res.getColor(R.color.colorFairway));
                     opt.strokeColor(res.getColor(R.color.colorFairway));
                     break;
                 case TYPE_GREEN:
+                    zIndex = 2.0f;
                     opt.fillColor(res.getColor(R.color.colorGreen));
                     opt.strokeColor(res.getColor(R.color.colorGreen));
                     break;
                 case TYPE_BUNKER:
+                    zIndex = 3.0f;
                     opt.fillColor(res.getColor(R.color.colorBunker));
                     opt.strokeColor(res.getColor(R.color.colorBunker));
                     break;
                 case TYPE_WATER:
+                    zIndex = 4.0f;
                     opt.fillColor(res.getColor(R.color.colorWater));
                     opt.strokeColor(res.getColor(R.color.colorWater));
                     break;
                 default:
+                    zIndex = 0.0f;
                     opt.fillColor(res.getColor(R.color.colorRough));
                     opt.strokeColor(res.getColor(R.color.colorRough));
             }
+            opt.zIndex(zIndex);
             map.addPolygon(opt);
         }
     }
